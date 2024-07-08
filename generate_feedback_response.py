fbcf5c7e0f169d8e9d0e3732bd7d832bc46d117a
Aleksandra Kukawka <48186382+olkuka@users.noreply.github.com>
2024-07-08 12:40:32 +0000

    latest fixes

--- generate_feedback_response.py
+++ generate_feedback_response.py
@@ -1,12 +1,23 @@
-from openai import OpenAI
+import requests
 import streamlit as st
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+API_TTS_ENDPOINT = st.secrets["OPENAI_TTS_ENDPOINT"]
 
-with client.audio.speech.with_streaming_response.create(
-        model = "tts-1",
-        voice = "alloy",
-        input = "Please start talking now.",
-    ) as audio_response:
-        audio_response.stream_to_file("feedback_response_start_talking.mp3")
-            
+try:
+    headers = {
+        "Content-Type": "application/json",
+        "api-key": st.secrets["OPENAI_API_KEY"],
+    }
+    payload = {
+        "model": "tts-1",
+        "voice": "alloy",
+        "input": "Generating the response.",
+    }
+    audio_response = requests.post(API_TTS_ENDPOINT, headers=headers, json=payload)
+    audio_response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+except requests.RequestException as e:
+    raise SystemExit(f"Failed to make the request. Error: {e}")
+
+# save output to mp3 file
+with open("feedback_response.mp3", "wb") as fout:
+    fout.write(audio_response.content)
